69d493c40bb8846e0b29b0168e804ae81dc3cb9b
@mulugetaassefa! <mulugetaassefa077@gmail.com>
2024-03-05 04:06:27 -0700

    api development using .NET7 and mongoDB database

---
+++ WebApiMongoDB/Data/DatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiMongoDB.Data
+{
+    public class DatabaseSettings
+    {
+        public string CollectionName { get; set; } =string.Empty;
+        public string DatabaseName { get; set; } =string.Empty;
+        public string Connection { get; set; } =string.Empty;
+    }
+}
---
+++ WebApiMongoDBApp/webapimongodbapp.client/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AppRoutes from './AppRoutes.jsx';
+import './custom.css';
+
+export default function App() {
+    return (
+        <Routes>
+          {AppRoutes.map((route, index) => {
+            const { element, ...rest } = route;
+            return <Route key={index} {...rest} element={element} />;
+          })}
+        </Routes>
+    );
+}
